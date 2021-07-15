c8ad3a305ae149c3fb90973cfbd1ea35e7988f2c
unknown <you@example.com>
2021-07-15 20:54:44 +0300

    nem ud ltd

--- ussd/ussd.php
+++ ussd/ussd.php
@@ -9,4 +9,17 @@
 
  $serviceCode - $_POST['serviceCode'];
 
- echo " Welcome to naha ug";
+ $counter = explode("*",$testFromUser);
+
+ if (empty($testFromUser)) {
+ 	
+ 	$response = "CON Welcome to the NEMA (u) limited";
+
+ 	$response = "\n 1. Register";
+
+ 	$response = "\n 2. Add a tree";
+
+ 	echo $response;
+ }else{
+     echo "END we have".$testFromUser;
+ }
