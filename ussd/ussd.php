09757a70cfe193e0f283fd51efd89deffc230fe2
unknown <you@example.com>
2021-07-15 19:11:25 +0300

    ussd

---
+++ ussd/ussd.php
@@ -0,0 +1,12 @@
+ <?php
+ header("content-type:text/plain");
+
+ $phone_number - $_POST['phoneNumber'];
+
+ $testFromUser - $_POST['text'];
+
+ $sessionID - $_POST['sessionId'];
+
+ $serviceCode - $_POST['serviceCode'];
+
+ echo "CON Welcome to naha ug";
