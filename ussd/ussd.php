299472ed932c3c7dc41dc79698df4cc2188d99a5
unknown <you@example.com>
2021-07-17 19:38:08 +0300

    finally

--- ussd/ussd.php
+++ ussd/ussd.php
@@ -1,78 +1,165 @@
  <?php
- header("content-type:text/plane");
+header("Content-type:text/plane");
 
- $phone_number - $_POST['phoneNumber'];
+require('../AfricasTalkingGateway.php');
 
- $testFromUser - $_POST['text'];
+require('sql_connection.php');
 
- $sessionID - $_POST['sessionId'];
+$phone_number = $_POST['phoneNumber'];
 
- $serviceCode - $_POST['serviceCode'];
+$textFromUser = $_POST['text'];
 
- if(empty($testFromUser)){
-  
-     $testFromUser = "0";
+$sessionID = $_POST['sessionId'];
 
- }else{
+$serviceCode = $_POST['serviceCode'];
 
-     $testFromUser = "0*".$testFromUser;
+if(empty($textFromUser)){
 
- }
+	$textFromUser = "0";
 
- $inputArray = explode("*",$testFromUser);
+}else{
 
- $level = count($inputArray);
+	$textFromUser = "0*".$textFromUser;
 
- switch ($level) {
-     
-     case 1: 
+}
 
-            $response = "CON Welcome to the NEMA (u) limited";
+$inputArray = explode("*", $textFromUser);
 
-            $response .= "\n 1. Register";
+$level = count($inputArray);
 
-            $response .= "\n 2. Add a tree";
+switch ($level) {
 
-            echo $response;
-        
-         break;
+	case 1:
 
-     case 2: //text =0*1 OR 0*2
+		$response = "CON Welcome to the NEMA (U) Limited";
 
-             if($inputArray[1] == 1) {//wants to register
-             
-             echo "CON What is your name?";
+	    $response .= "\n 1. Register";
+
+	    $response .= "\n 2. Add a tree";
+
+	    echo $response;
+
+		break;
+
+	case 2:		// text = 0*1  OR  0*2 
+
+		 if($inputArray[1]   ==  1) {//he wants to register// 
+
+		 	echo "CON What is is your name?";
 
 
-             }elseif ($inputArray[1] == 2) {//wants to add tree
-              
-             }else{
 
-                echo "End invalid option";
-             }
+		 }elseif ($inputArray[1] == 2) {//he wants to add a tree
 
-             
+		 	$checkmembers = $sqliCon->query("SELECT * FROM members WHERE phone_number = '$phone_number' ");
 
-             break;
+		 	if($checkmembers->num_rows == 0) 
 
-      case 3:
-                
+		 		echo "END User with phone_number $phone_number has no account";
 
-                 break;       
-     
-     default:
-         
-         echo "The option selected is not valid";
-         
+		 	else{
 
-         break;
- }
+		 		while ($results = $checkmembers->fetch_assoc()) {
 
+		 			echo "CON ".$results['name']."\n Enter the number of trees";
+
+		 		}
+
+		 	}
+				
+					  
+
+		 
+		 }else{
+
+		 	echo "END Invalid option";
+
+		 }
+		  
+		break;
+
+	case 3: 
+
+	     if($inputArray[1]   ==  1) {//he wants to register// // 0*1*
+
+		 	$user_name = $inputArray[2];
+
+		 	$saveUser = $sqliCon->query("INSERT INTO members(phone_number,name)VALUES('$phone_number','$user_name')");
+
+		 	if($saveUser){
+
+		 		$message = "Hello ".$user_name." Thank you for registering with NEMA (U) ltd";		        
+				$apikey     = "8ca31226367ab4abde28fc34a62a2ef852d0e730b66c02348c98ed7499ca087c";			 
+				$gateway    = new AfricasTalkingGateway("sandbox", $apikey,"sandbox");
+
+				$gateway->sendMessage($phone_number, $message); 
+
+				echo "END Thank you for registering"; 
+
+		 	}else{
+
+		 		echo "END Failed to register ".$sqliCon->error;
+
+		 	}
+
+
+		 }elseif ($inputArray[1] == 2) {//he wants to add a tree// // 0*2*7837
+
+		 	$number_of_trees = $inputArray[2];
+
+		 	$checkmembers = $sqliCon->query("SELECT id,name FROM members WHERE phone_number = '$phone_number'");
+
+		 	if($checkmembers->num_rows == 1){
+		 
+		 	while ($rows = $checkmembers->fetch_assoc()) {
+
+		 		$member_id = $rows['id'];
+
+		 	    $member_name = $rows['name'];
+
+		 	    $sqliCon->query("INSERT INTO trees(member_id,number_of_trees)VALUES('$member_id','$number_of_trees')");
+
+		 	    $message = "Hello $member_name, Thank you for conserving the Forests of Uganda. You have recorded $number_of_trees tree(s)";
+			    $apikey     = "";			 
+			    $gateway    = new AfricasTalkingGateway("sandbox", $apikey,"sandbox");
+			    $gateway->sendMessage($phone_number, $message);
+		 	    echo "END $message";
+		 		 
+		 	}
+
+		 }else{
+
+		 	echo "END No user found";
+
+		 }
+	 
+	}  
+
+	 
+		break;
+	
+	default:
+
+		echo"The option selected is not valid";
+
+		break;
+}
+
+
+
+
+// Welcome message 
+
+// Choose an option 
+
+//   1. Register 
+//   2. Add a tree 
  
- // if (empty($testFromUser)) {
- 	
- 
- 
- // }else{
- //     echo "END we have".$testFromUser;
- // }
+
+// 1.  WHat is your name?
+//     send them sms that they have registered 
+
+// 2.  Check id they have registered 
+//     Enter number of of trees 
+//      Send a thank you message
+
